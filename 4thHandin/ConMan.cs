5ad90aaab144dcc3f6a823fc6a5ec1d1cecdd774
Stanislav <stanislavjamnicky@gmail.com>
2018-11-30 10:39:51 +0100

    DisplayTop10 and Search Update

---
+++ 4thHandin/ConMan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace _4thHandin
+{
+    public class ConMan
+    {
+        //public static SqlConnection ConnecStr = new SqlConnection(@"data source = ANDREAS2018-PC\SQLEXPRESS; integrated security = true; database = Exterminatus;");
+        //public static SqlConnection ConnecStr = new SqlConnection(@"data source = MIHNEA-LAPTOP; integrated security = true; database = MovieDBList;");
+        public static SqlConnection ConnecStr = new SqlConnection(@"data source = THE-STAN; integrated security = true; database = MovieDBList;");
+        //public static SqlConnection ConnecStr = new SqlConnection(@"data source = DESKTOP-SCRFL8B; integrated security = true; database = MovieDBList;");
+
+        //add your own connection for easy access and comment out any others
+    }
+}
